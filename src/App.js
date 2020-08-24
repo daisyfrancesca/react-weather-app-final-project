1d3659dd746f32056d297c33367898780d4810b3
daisyfrancesca <65689227+daisyfrancesca@users.noreply.github.com>
2020-08-23 21:20:57 -0400

    Added Footer - GitHub and Linkedin Urls

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Daisy's Weather App</div>;
+  return (
+    <div className="App">
+      Daisy's Weather App
+      <footer>
+        <a
+          href="https://github.com/daisyfrancesca/react-weather-app-final-project"
+          target="_blank"
+        >
+          <p>Open Source Code</p>
+        </a>
+        <a href="https://www.linkedin.com/in/daisyfrancesca/" target="_blank">
+          <p>Created by Daisy Simpson</p>
+        </a>
+      </footer>
+    </div>
+  );
 }
