60939eab4ba392bffa6262fffb8581eeca7b26fc
daisyfrancesca <65689227+daisyfrancesca@users.noreply.github.com>
2020-08-24 20:49:39 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Daisy's Weather App
-      <footer>
-        <a
-          href="https://github.com/daisyfrancesca/react-weather-app-final-project"
-          target="_blank"
-        >
-          <p>Open Source Code</p>
-        </a>
-        <a href="https://www.linkedin.com/in/daisyfrancesca/" target="_blank">
-          <p>Created by Daisy Simpson</p>
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Daisy's Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/daisyfrancesca/react-weather-app-final-project"
+            target="_blank"
+          >
+            <p>Open Source Code</p>
+          </a>
+          <a href="https://www.linkedin.com/in/daisyfrancesca/" target="_blank">
+            <p>Created by Daisy Simpson</p>
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
