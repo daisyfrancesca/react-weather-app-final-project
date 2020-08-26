62fa29f03a95ed5bb6302a8deeb91961d6c2bc4a
daisyfrancesca <65689227+daisyfrancesca@users.noreply.github.com>
2020-08-26 19:14:43 -0400

    Updated columns to be more responsive

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { ReactComponent as Logo } from "./logo.svg";
+import axios from "axios";
 
 export default function weather() {
+  const apiKey = "d643ee59f43b44ad31e57464532264d8";
+  let city = "London";
+  let apiURL = `http://api.openweahtermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   return (
     <div>
       <div class="row">
-        <div className="col-md-6">
+        <div className="col">
           <Logo />
         </div>
-        <div className="col-6">
+        <div className="col">
           <ul className="temp">
             <span className="temperature"> 76 </span>
             <span className="unit">
