3a7af356416076d8bdee8ecb47076bf0cbc00994
daisyfrancesca <65689227+daisyfrancesca@users.noreply.github.com>
2020-09-04 06:25:59 -0400

    Updated api URL

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -39,7 +39,7 @@ export default function WeatherForecast(props) {
     );
   } else {
     let apiKey = "d643ee59f43b44ad31e57464532264d8";
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=imperial`;
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=imperial`;
     axios.get(url).then(handleForecastResponse);
     return null;
   }
